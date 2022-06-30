6a58452c9fee68a1d0a6ea298b7a8e24ceab3fd9
Kirill Ramzaev <kerah304@gmail.com>
2022-06-30 18:03:27 +0300

    Try...

--- src/components/MuiSpeedDial.tsx
+++ src/components/MuiSpeedDial.tsx
@@ -2,33 +2,32 @@ import * as React from 'react';
 import SpeedDial from '@mui/material/SpeedDial';
 import HelpIcon from '@mui/icons-material/Help';
 import { useState } from 'react'
-import Popover from '@mui/material/Popover';
+import Popover from '@reactour/tour';
 
     const MuiSpeedDial = () => {
-    const [open, setIsOpen] = useState(false)
-    const handleClick = () => setIsOpen(true);
-    const handleClose = () => setIsOpen(false);
-    
+    const [isOpen, setIsOpen] = useState(false)
   
   return (
         <SpeedDial 
         ariaLabel="Navigation speed dial"
         sx={{ position: 'absolute', bottom: 20, right: 16 }}
         icon={<HelpIcon />}
-        onClose={handleClose}
-        onOpen={handleClick}
-        
+        onClick={() => setIsOpen((o)=>!o)}
         >
-        <Popover
-        open={open}
-        onClose={handleClose}
-        anchorReference="anchorPosition"
-        anchorPosition={{ top: 300, left: 700 }}
-     
-      >
-        <h1>Hello World</h1>
-        </Popover>
+            {isOpen ?(
+            <Popover position="center">
+            <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            Ab minima numquam vel dolore amet quos iure at! Aut, nisi eaque dignissimos 
+            totam officiis soluta quod ullam nostrum excepturi, possimus ratione.
+            </p>
+            </Popover>
+        ):null}
+
+        
         </SpeedDial>
+        
+        
         
         
         
