3886b75124c7290ed09dfa7820b5a7d059339539
Kirill Ramzaev <kerah304@gmail.com>
2022-06-30 15:33:16 +0300

    Try to add Popover

--- src/components/MuiSpeedDial.tsx
+++ src/components/MuiSpeedDial.tsx
@@ -1,13 +1,35 @@
+import * as React from 'react';
 import SpeedDial from '@mui/material/SpeedDial';
 import HelpIcon from '@mui/icons-material/Help';
+import { useState } from 'react'
+import Popover from '@mui/material/Popover';
 
 export const MuiSpeedDial = () => {
-    return (
+    const [open, setIsOpen] = useState(false)
+    const handleClick = () => setIsOpen(true);
+    const handleClose = () => setIsOpen(false);
+    
+  
+  return (
         <SpeedDial 
         ariaLabel="Navigation speed dial"
         sx={{ position: 'absolute', bottom: 20, right: 16 }}
         icon={<HelpIcon />}
-        >     
+        onClose={handleClose}
+        onOpen={handleClick}
+        
+        >
+        <Popover
+        open={open}
+        onClose={handleClose}
+        anchorReference="anchorPosition"
+        anchorPosition={{ top: 300, left: 700 }}
+     
+      >
+        <h1>Hello World</h1>
+        </Popover>
         </SpeedDial>
+        
+        
     )
 }
