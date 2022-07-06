2ccaa6e94f8eea41488df555a2c4c49991c618b2
Kirill Ramzaev <kerah304@gmail.com>
2022-07-06 14:07:16 +0300

    Del MuiSpeedDial.css

--- src/components/UI/SpeedDial/MuiSpeedDial.tsx
+++ src/components/UI/SpeedDial/MuiSpeedDial.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import SpeedDial from '@mui/material/SpeedDial';
 import HelpIcon from '@mui/icons-material/Help';
 import { useTour } from '@reactour/tour'
-import classes from './MuiSpeedDial.module.css';
 
 
 const MuiSpeedDial = () => {
@@ -11,7 +10,6 @@ const { setIsOpen } = useTour()
     return (
         
         <SpeedDial
-        className={classes.MuiSpd} 
         ariaLabel="Navigation speed dial"
         sx={{ position: 'absolute', bottom: -35, right:-75}}
         icon={<HelpIcon />}
